ce2666b54cacab7e11a0cb5a99b1711afac77c60
Luis Louro <lapisdecor@gmail.com>
2025-09-12 06:38:21 +0100

    Add the game

---
+++ src/tabuada.nim
@@ -0,0 +1,29 @@
+# This is just a simple terminal game to test
+# your multiplication tables knowledge
+
+import std/random, std/strformat, std/strbasics
+
+proc start = 
+  echo("Type q to stop")
+  while true:
+    randomize()
+    var a = rand(2..9)
+    var b = rand(2..9)
+    var c  = a * b
+    var d = $c   
+    echo("{a} x {b} = ?".fmt)
+    var consoleInput = readLine(stdin)
+    strip(consoleInput)
+    if consoleInput == "q":
+      echo("Bye!\n".fmt)
+      break
+    if consoleInput == d:
+      echo("Correct!\n".fmt)
+    else:
+      echo("Incorrect. It was {d}.".fmt)
+
+    echo("\n")
+
+  
+when isMainModule:
+  start()
---
+++ tabuada.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "Luis Louro"
+description   = "A simple game to learn multiplication tables"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["tabuada"]
+
+
+# Dependencies
+
+requires "nim >= 2.2.4"
